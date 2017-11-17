94836ca7fdccf78c12dfa8f69c273180e684f395
Jack Pearson <jackwpearson1@gmail.com>
2017-11-17 14:16:41 -0700

    Jacks index.js commit

--- server/index.js
+++ server/index.js
@@ -8,9 +8,12 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+massive(process.env.CONNECTION_STRING).then(datab => app.set('datab', datab));
 
 
 // LOGIN
+app.post('/api/Login/:id', login_controller.addLogin)
+app.get('/api/Login/:id', login_controller.getLogin)
 
 //LISTINGS
 app.post('/api/Listings/:id', listings_controller.addListing)
@@ -18,11 +21,11 @@ app.get('/api/Listings/:id', listings_controller.getListing)
 app.delete('/api/Listings/:id', listings_controller.deleteListing)
 //DETAILS
 app.post('/api/Details/:id', details_controller.addDetails)
+app.get('/api/Details/:id', details_controller.getDetails)
+app.delete('/api/Details/:id', details_controller.deleteDetials)
 
 
 
 
-
-
-const port = process.env.PORT || 3000;
+const port = process.env.Port || 3000;
 app.listen(port, ()=> {console.log(`Server listening on port ${port}.`)})
